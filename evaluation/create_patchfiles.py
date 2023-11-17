c7e422a57b8f0fd89633d305a649cb13ce30fe6e
tillwenke <till.wenke@gmx.net>
2023-11-17 17:33:08 +0100

    prep uploading eval data

---
+++ evaluation/create_patchfiles.py
@@ -0,0 +1,82 @@
+import sys, argparse, logging, os
+import pandas as pd
+from utils import *
+
+
+# Gather our code in a main() function
+def main(args, loglevel):
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)    
+
+    logging.info(f"Choose unevaluated PR number from https://docs.google.com/spreadsheets/d/1KU2sSywl2VTK6JdVa73eC9r1UbhtMP9yaKb2gF5qmw0/edit?usp=sharing")
+    logging.info(f"And document your findings there.")
+    logging.info(f"Feel free to add new columns if you want to point towards something that does not fit in the existing columns.")
+    pr_number = input("PR number: ")
+
+    df = pd.read_csv("../data/cwa_server/evaluation.csv", index_col="id")
+    this = df.loc[int(pr_number)]
+
+    logging.info("Creating patch files.")
+    # create temporary patch files
+    patch = this["patch"]
+    with open("./tmp/real.patch", "w") as diff_file:
+        diff_file.write(patch)
+
+    patch = this["prediction"]
+    with open("./tmp/predicted.patch", "w") as diff_file:
+        diff_file.write(patch)
+
+    # view create prs for viewing
+    view_prs(
+        this["base_commit"],
+        "../SWE-bench/evaluation/tmp/real.patch",
+        "../SWE-bench/evaluation/tmp/predicted.patch",
+        "../../cwa-server/",
+        pr_number,
+    )
+
+    logging.info(f"Original PR that also includes changed test cases.")
+    logging.info(this.pr_link)
+    logging.info(f"Original issue.")
+    logging.info(this.issue_link)
+
+    logging.info("Done with reviewing PRs? Mark as done in Google Sheet.")
+    logging.info("'Enter' to continue.")
+    input()
+
+    # delete
+    # TODO dont kown why up once instead of twice is sufficient
+    cleanup_repo("../cwa-server/", pr_number)
+
+    # delete patch files
+    os.chdir("../SWE-bench/evaluation/")
+    os.remove("./tmp/real.patch")
+    os.remove("./tmp/predicted.patch")
+
+    logging.info("Done")
+
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Does a thing to some stuff.",
+        epilog="As an alternative to the commandline, params can be placed in a file, one per line, and specified on the commandline like '%(prog)s @params.conf'.",
+        fromfile_prefix_chars="@",
+    )
+
+    # parser.add_argument(
+    #     "pr_number", help="pass number of PR to the program", metavar="pr_number"
+    # )
+
+    parser.add_argument(
+        "-v", "--verbose", help="increase output verbosity", action="store_true"
+    )
+    args = parser.parse_args()
+
+    # Setup logging
+    if args.verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+
+    main(args, loglevel)
---
+++ evaluation/utils.py
@@ -0,0 +1,113 @@
+import re
+
+from git import Repo
+
+from git.exc import GitCommandError
+import sys
+import os
+
+
+def find_files(text):
+    files = []
+    regex = "\[start of (.+?)\]"
+    m = re.search(regex, text)
+    while True:
+        files.append(m.group(1))
+        text = text.replace(m.group(0), "")
+        m = re.search(regex, text)
+        if m is None:
+            break
+    return files
+
+
+def view_prs(
+    base_commit: str,
+    real_patch_path: str,
+    predicted_patch_path: str,
+    repo_path: str,
+    pr_number: int,
+):
+    print("Creating PRs...")
+
+    viewing_branch = f"viewing_branch_{pr_number}"
+    real_patch_branch = f"real_patch_branch_{pr_number}"
+    pred_patch_branch = f"pred_patch_branch_{pr_number}"
+
+    repo = Repo.init(repo_path)
+    git = repo.git
+
+    # go to main branch
+    git.fetch("origin")
+    git.reset("--hard", "origin/main")
+    git.switch("main")
+
+    # go to the base commit and create all needed branches from it
+    git.checkout(base_commit)
+    git.branch(viewing_branch)
+    git.branch(real_patch_branch)
+    git.branch(pred_patch_branch)
+    
+    # treat all three branches
+    git.switch(viewing_branch)
+    git.push("--set-upstream", "origin", viewing_branch)
+
+    # go to branches to create a pr from
+    # apply code changes
+    git.switch(real_patch_branch)
+    git.apply(real_patch_path, "--whitespace=fix")
+    git.commit("--all", "--message=applied real patch")
+    git.push("--set-upstream", "origin", real_patch_branch)
+    
+    try:
+        git.switch(pred_patch_branch)
+        git.apply(predicted_patch_path, "--whitespace=fix")
+        git.commit("--all", "--message=apply predicted patch")
+        git.push("--set-upstream", "origin", pred_patch_branch)
+        os.chdir(repo_path)
+
+        # create prs
+        os.system(
+            f'gh pr create --title "view real patch" --body "." --base {viewing_branch} --head {real_patch_branch}'
+        )
+        os.system(
+            f'gh pr create --title "view predicted patch" --body "." --base {viewing_branch} --head {pred_patch_branch}'
+        )
+
+        print(f"\nSuccessfully created PRs! See them on github.")
+        print(f"Branch patches should be merged to.")
+        print(f"https://github.com/feedback-to-code/cwa-server/tree/{viewing_branch}")
+        print(f"You can find links to the patch PRs above.\n")
+
+    except GitCommandError as e:
+        print(e)
+        print("Could not apply predicted patch.")
+
+        # cleanup?
+
+
+def cleanup_repo(repo_path: str, pr_number: int):
+    viewing_branch = f"viewing_branch_{pr_number}"
+    real_patch_branch = f"real_patch_branch_{pr_number}"
+    pred_patch_branch = f"pred_patch_branch_{pr_number}"
+
+    repo = Repo.init(repo_path)
+    git = repo.git
+
+    # delete branches on github - closes prs automatically
+    git.push("origin", "--delete", viewing_branch)
+    git.push("origin", "--delete", real_patch_branch)
+    try:
+        git.push("origin", "--delete", pred_patch_branch)
+    except:
+        print("Could not delete pred_patch_branch remote. It probably does not exist.")
+
+    # delete branches locally
+    git.checkout("main")
+    git.branch("-D", viewing_branch)
+    git.branch("-D", real_patch_branch)
+    try:
+        git.branch("-D", pred_patch_branch)
+    except:
+        print("Could not delete pred_patch_branch locally. It probably does not exist.")
+
+    print("Successfully cleaned up!")
